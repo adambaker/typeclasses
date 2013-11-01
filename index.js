00235c8e4a67ecb491f525c262a98db192327fa2
Adam C. Baker <adam.durandal@gmail.com>
2013-10-31 18:30:10 -0700

    extract utilities into util, grab it with es6 syntax

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-require('./support/types');
+import {autoCurry} from './util'
+
+var hasAC == Boolean(Function.prototype.autoCurry);
+
+if(!hasAC) {
+  //the rest of the lib wants autoCurry on Function's prototype
+  //this sucks, but I'll clean it up later
+  Function.prototype.autoCurry = function(n) {
+    return autoCurry(this, n);
+  }
+}
+
 require('./functor');
 require('./applicative');
 require('./monoid');
 require('./monad');
 require('./promise');
+
+if(!hasAC) {
+  //remove autoCurry from function's prototype so that it
+  //doesn't affect users
+  delete Function.prototype.autoCurry
+}
+
+
---
+++ util.js
@@ -0,0 +1,54 @@
+
+export var Constructor = function(f) {
+  var x = function(){
+    if(!(this instanceof x)){
+      var inst = new x();
+      f.apply(inst, arguments);
+      return inst;
+    }
+    f.apply(this, arguments);
+  };
+
+  return x;
+};
+
+export var makeType = function(f) {
+  f = f || function(v){ this.val = v; }
+  return Constructor(f);
+};
+
+var toArray = function(x) {
+  return Array.prototype.slice.call(x);
+}
+
+var curry = function (fn /* variadic number of args */) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  var f = function () {
+    return fn.apply(this, args.concat(toArray(arguments)));
+  };
+  return f;
+};
+
+export var autoCurry = function (fn, numArgs) {
+  numArgs = numArgs || fn.length;
+  var f = function () {
+    if (arguments.length < numArgs) {
+      return numArgs - arguments.length > 0 ?
+        autoCurry(curry.apply(this, [fn].concat(toArray(arguments))),
+            numArgs - arguments.length) :
+        curry.apply(this, [fn].concat(toArray(arguments)));
+    }
+    else {
+      return fn.apply(this, arguments);
+    }
+  };
+  f.toString = function(){ return fn.toString(); };
+  f.curried = true;
+  f.fn = fn;
+  f.arity = fn.length;
+  return f;
+};
+
+Function.prototype.autoCurry = function(n) {
+  return autoCurry(this, n);
+}
